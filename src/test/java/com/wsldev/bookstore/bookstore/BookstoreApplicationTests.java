ddbe3b80408158e12fa27d7751a2dba327efa2cb
wslmacieira <wslmacieira@outlook.com>
2021-12-27 14:04:26 -0300

    commit inicial

---
+++ src/test/java/com/wsldev/bookstore/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsldev.bookstore.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
